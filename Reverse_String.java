0dbe7ffccf1e6f4488f63a4d44bc503222660e23
Viswin <94142820+Column9@users.noreply.github.com>
2026-05-26 18:03:14 -0400

    Add files via upload

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,8 +1,11 @@
 public class Reverse_String {
     public static void main(String[] args) {
         String s = "Vis";
+        String reverse = "";
         for(int i = s.length() - 1; i >= 0; i--) {
-            System.out.println(s);
+            reverse += s.charAt(i);
         }
+        System.out.println(reverse);
     }
+
 }
